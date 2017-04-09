6c70de602e1360027d63fae6194b87fe182e4552
Etile9998 <gosehwan@gmail.com>
2017-04-09 19:11:02 +0900

    Create base.cpp

---
+++ src/base.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define mp make_pair
+#define mod 1000000007
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+#ifdef _CONSOLE
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+#endif
+    // your code goes here
+
+    return 0;
+}
