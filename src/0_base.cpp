c79adaa0091069281a2c87c4ef167445da14692b
Etile9998 <gosehwan@gmail.com>
2017-04-22 17:15:56 +0900

    Update 0_base.cpp

--- src/0_base.cpp
+++ src/0_base.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
-#include <cstdio>
+#ifdef _CONSOLE
+#include "bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
 using namespace std;
 
 typedef long long ll;
@@ -8,7 +11,7 @@ typedef long long ll;
 #define mp make_pair
 #define fi first
 #define se second
-#define mod 1000000007
+#define mod 1e9+7
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0);
@@ -17,6 +20,6 @@ int main() {
     freopen("test.out", "w", stdout);
 #endif
     // your code goes here
-
+    
     return 0;
 }
