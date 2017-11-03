55149499ae7af0a8cba6a7e26bd61ea8f2a28693
Etile9998 <gosehwan@gmail.com>
2017-11-04 00:44:39 +0900

    Update 0_base.cpp

--- src/0_base.cpp
+++ src/0_base.cpp
@@ -1,26 +1,21 @@
-#ifdef _CONSOLE
-#include "bits/stdc++.h"
-#else
 #include <bits/stdc++.h>
-#endif
 using namespace std;
 
-typedef long long ll;
+using ll = long long;
 
 #define rep(i,a,b) for(int i = a; i < b; ++i)
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
-#define mod 1000000007
+const int mod = 1e9 + 7;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0);
+	ios::sync_with_stdio(false); cin.tie(0);
 #ifdef _CONSOLE
-    freopen("sample.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
 #endif
-    // your code goes here
-    
-    return 0;
+	// your code goes here
+
+	return 0;
 }
