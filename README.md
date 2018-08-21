9341e9455ecfd2ee7529fda2f55bba15a6455692
Etile9998 <gosehwan@gmail.com>
2018-08-21 16:52:10 +0900

    대강대강

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# algorithm
+## algorithm
 
 자주 쓰는 코드 모음
+
+
+    :r src/template.cpp
+    로 기본 템플릿 가져오고
+    코드 작성하면 됨 ㅇㅇ
--- src/timer.cpp
+++ src/timer.cpp
@@ -1,15 +1,15 @@
 #include <chrono>
 
+using namespace std::chrono;
 class Timer{
 private:
-    using clock_t = std::chrono::steady_clock;
-    using second_t = std::chrono::duration<double, std::ratio<1>>;
-
-    std::chrono::time_point<clock_t> startTime = clock_t::now();
-
+    steady_clock::time_point t1 = steady_clock::now();
+    
 public:
     void elapsed(){
-        std::chrono::time_point<clock_t> endTime = clock_t::now();
-        std::cout << std::chrono::duration<second_t>(endTime - startTime).count() << std::endl;
+        steady_clock::time_point t2 = steady_clock::now();
+        duration<double> time_span = duration_cast<double>>(t2 - t1);
+
+        std::cout << time_span.count() << '\n';
     }
-}
+};
