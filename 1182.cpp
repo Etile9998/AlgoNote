1a8eb90c9f79f94046a73b30244681c2af05a4e6
etile9998 <gosehwan@gmail.com>
2019-06-11 14:52:01 +0900

    또잉또잉

--- 1182.cpp
+++ 1182.cpp
@@ -3,11 +3,26 @@ using namespace std;
 
 int n;
 int s;
+int bits;
+int a[20];
+int len;
+int ans;
 
 int main(){
     cin >> n >> s;
     for (int i = 0 ; i < n ; i++){
-         
+        cin >> a[i];                 
     }
+    len = (1 << n);
+    for (int i = 1 ; i < len ; ++i){
+        int tmp = 0;
+        for (int j = 0 ; j < n ; j++){
+            if ((1 << j) & i){
+                tmp += a[j];
+            }
+        }
+        if (tmp == s) ans += 1;
+    }
+    cout << ans << '\n';
     return 0;
 }
